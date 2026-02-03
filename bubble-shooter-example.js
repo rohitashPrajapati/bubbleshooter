01ba47ce5b37df354d49eb5416fdef0ab435dc89
Rohitash Prajapati <rohitash.prajapati@isol.co.in>
2026-02-03 17:43:28 +0530

    Applied the button image for pause-resume and sound on-off.

--- bubble-shooter-example.js
+++ bubble-shooter-example.js
@@ -1814,30 +1814,38 @@ window.onload = function() {
 
     // Pause/Resume button logic
     if (pauseBtn && pausedOverlay && resumeBtn) {
+        var pauseImg = pauseBtn.querySelector("img");
+        
         pauseBtn.onclick = function() {
-            isPaused = true;
-            pausedOverlay.style.display = "flex";
-            pauseBtn.style.display = "none";
+            isPaused = !isPaused;
+            if (isPaused) {
+                pausedOverlay.style.display = "flex";
+                if (pauseImg) pauseImg.src = "images/play_button-min.png";
+            } else {
+                pausedOverlay.style.display = "none";
+                if (pauseImg) pauseImg.src = "images/pause-min.png";
+            }
         };
+
         resumeBtn.onclick = function() {
             isPaused = false;
             pausedOverlay.style.display = "none";
-            pauseBtn.style.display = "inline-block";
+            if (pauseImg) pauseImg.src = "images/pause-min.png";
         };
     }
-
-    // SVGs for sound on/off
-    var soundOnSVG = '<svg id="sound-on-svg" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" style="display:block; color:#3b59ff;"><path d="M20.522 7.228a6.74 6.74 0 0 1 0 9.544M7.5 15.75H3a.75.75 0 0 1-.75-.75V9A.75.75 0 0 1 3 8.25h4.5L14.25 3v18L7.5 15.75Zm0-7.5v7.5m10.369-5.869a2.99 2.99 0 0 1 0 4.238" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
-    var soundOffSVG = '<svg id="sound-off-svg" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="28" height="28" style="display:block; color:#3b59ff;"><path d="M27.363 9.637a8.988 8.988 0 0 1 0 12.726M10 11v10m13.825-7.825a3.99 3.99 0 0 1 0 5.65M6 5l20 22m-7-7.7V28l-9-7H4a1 1 0 0 1-1-1v-8a1 1 0 0 1 1-1h6l.85-.662m3.175-2.463L19 4v9.35" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
 
     // Sound on/off button logic
     if (soundBtn && soundIcon) {
         soundBtn.onclick = function() {
             soundEnabled = !soundEnabled;
-            soundIcon.innerHTML = soundEnabled ? soundOnSVG : soundOffSVG;
+            soundIcon.innerHTML = soundEnabled ? 
+                '<img id="sound-on-img" src="images/sound_on-min.png" alt="Sound On" style="width: 45px; height: auto; display: block;">' : 
+                '<img id="sound-off-img" src="images/sound_off-min.png" alt="Sound Off" style="width: 45px; height: auto; display: block;">';
         };
         // Ensure initial icon is correct
-        soundIcon.innerHTML = soundEnabled ? soundOnSVG : soundOffSVG;
+        soundIcon.innerHTML = soundEnabled ? 
+            '<img id="sound-on-img" src="images/sound_on-min.png" alt="Sound On" style="width: 45px; height: auto; display: block;">' : 
+            '<img id="sound-off-img" src="images/sound_off-min.png" alt="Sound Off" style="width: 45px; height: auto; display: block;">';
     }
 
     // Email Popup Logic
